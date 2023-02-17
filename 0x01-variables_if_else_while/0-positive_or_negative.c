7f4ad7332f64b966cacad63c28da8ca18b28dc5f
Sherif Hamdy <445585913@g102019.moe.edu.eg>
2023-02-17 22:15:50 +0200

    here

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,11 +4,12 @@
 
 
 /**
- ** main - Entry point
- **
- ** Description: print value of n satus; zero, positive or negative
- ** Return: Always 0 (Success)
- */
+ * main - Entry point
+ *
+ * Description: print value of n satus; zero, positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
@@ -16,6 +17,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	/*my code*/
 	if (n > 0)
 		printf("%i is positive\n", n);
@@ -23,5 +25,6 @@ int main(void)
 		printf("%i is zero\n", n);
 	else
 		printf("%i is negative\n", n);
+
 	return (0);
 }
