418a08dff6dd1d9acd20064e42d10097cf6aacc7
root <445585913@g102019.moe.edu.eg>
2023-02-23 16:38:30 -0800

    good one

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,8 +3,7 @@
  *
  * Description: it checks if it's number
  *
- * Return: (0) if it's not a number
- * 		(1) if it's a number
+ * Return: (0) if it's not a number, (1) if it's a number
  */
 
 int _isdigit(int c)
