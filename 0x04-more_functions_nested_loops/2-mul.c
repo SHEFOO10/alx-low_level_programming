1ca84c37679873b067db49c0e95c498433171beb
root <445585913@g102019.moe.edu.eg>
2023-02-23 16:53:24 -0800

    good one

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * multiplication - multiply two numbers
+ *
+ * Discription: takes two numbers as argument to mulitply 
+ *
+ * Return: result of multiplication
+ * 
+ */
+
+int mul(int a, int b)
+{
+	return a * b;
+}
