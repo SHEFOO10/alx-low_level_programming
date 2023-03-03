db7f9e1461918f6291673bcf4043e6ea1fb19a61
root <445585913@g102019.moe.edu.eg>
2023-03-03 08:53:00 -0800

    fixed

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+/**
+ * _strcat(char *dest, char *src) - concatenate string to another one
+ *
+ * @dest: take the destination variable that we will add to it another string
+ * @src: this variable will be add to destination variable
+ *
+ *
+ * Description: Concatenate string with another
+ *
+ * Return: string
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	while(*dest != '\0')
+	{
+		dest++;
+	}
+
+	*dest = ' ';
+	dest++;
+	while(*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+
+	}
+
+	*dest = '\0';
+
+	return dest;
+}
