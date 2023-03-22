7a65116cce61955b64860c03034c92f31b975b4c
root <445585913@g102019.moe.edu.eg>
2023-03-22 10:17:38 -0700

    new

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,14 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
 /**
- * print_name - print name
- *
- * @name: name
- * @f: function
- *
- * Return: Nothing
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
  */
-
-
-
-
 void print_name(char *name, void (*f)(char *))
 {
-
-	f(*name);
+	if (!name || !f)
+		return;
+	f(name);
 }
