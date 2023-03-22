a532155348e92b0041a3fd8cc52aa4b3cc9e5fd7
root <445585913@g102019.moe.edu.eg>
2023-03-22 10:32:08 -0700

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,36 @@
+/**
+ * array_iterator - looping on array
+ *
+ * @array: the array
+ * @size: the size
+ * @action: the action
+ *
+ * Return: Nothing
+ */
+
+
+
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int n = 0;
+
+	if( !array || !size || !action())
+		return (0);
+
+
+	while(n != size)
+	{
+		(*action)(array[n]);
+		n++;
+	}
+}
+
+
+
+
+
+
+
+
+}
