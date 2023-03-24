645ffacc7cf0d9f015cca88b5b9e456b1dbd7b44
root <445585913@g102019.moe.edu.eg>
2023-03-23 17:25:25 -0700

    good one

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -28,14 +28,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", str);
 		else if (separator && i == 0)
 			printf("%s", str);
-		else 
+		else
 			printf("%s%s", separator, str);
 
 	}
 
-	
+
 	printf("\n");
-	
+
 	va_end(str_list);
 }
 
