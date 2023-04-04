f2c0e5d0a5004649f1e62386249ab12c66b9a827
root <445585913@g102019.moe.edu.eg>
2023-04-04 16:39:44 -0700

    good one

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,16 +12,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t length = 0;
 
-	while (h->next)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		length++;
+		h->next;
 	}
 
-	length++;
-
-	return (length);	
-
-
-
+	return (length);
 }
