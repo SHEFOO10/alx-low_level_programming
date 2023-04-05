470059060a35a96ed51b107cf2d42e17c350dd50
root <445585913@g102019.moe.edu.eg>
2023-04-05 11:34:00 -0700

    add temp pointer

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,14 +11,20 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *temp = *head;
 	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *temp = *head;
 
 	if (!new)
 		return (NULL);
 
-	while (temp)
-		temp->next;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (temp->next)
+		temp = temp->next;
 
 	new->n = n;
 	new->next = NULL;
