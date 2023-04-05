d4703d6dfdafb01075cbd578f9ab39100e0aa078
root <445585913@g102019.moe.edu.eg>
2023-04-05 15:49:33 -0700

    remove asteric

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int loop_index = 0;
-	listint_t *temp = *head;
+	listint_t *temp = head;
 
 	if (!temp && !index)
 		return (NULL);
