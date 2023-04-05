3e1c50acc32d873196a7686346c1253af1c2ddfa
root <445585913@g102019.moe.edu.eg>
2023-04-05 15:33:49 -0700

    Get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,6 +13,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int loop_index = 0;
 
+	if (!head)
+		return (NULL);
 	while (head)
 	{
 		if (loop_index == index)
