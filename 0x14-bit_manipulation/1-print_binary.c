9ce5a1080800908b831d2898a21f80dc6eb53c89
root <445585913@g102019.moe.edu.eg>
2023-04-11 16:32:47 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,6 +10,16 @@
 
 void print_binary(unsigned long int n)
 {
-	printf("%b", n);
+	short int mask = 1 << 7;
+
+	while (mask > 0)
+	{
+		if (n & mask)
+			_putchar('1');
+		else
+			_putchar('0');
+
+		mask >>= 1;
+	}
 
 }
