9a442df061af0763ada99b6cf9b3a9e673127ef8
root <445585913@g102019.moe.edu.eg>
2023-04-10 19:31:10 -0700

    trin' something

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,34 +10,6 @@
 
 void print_binary(unsigned long int n)
 {
-	int len = n / 2;
-	char *b_number = malloc(8);
-	char *ptr = b_number;
-	int i;
-	int reminder = 0;
-
-	for (i = 6; i >= 0; i--)
-	{
-		if (i <= len)
-			break;
-
-		n = n / 2;
-		reminder = n % 2;
-		if (reminder)
-		{
-			*b_number = '1';
-			b_number++;
-		}
-		else
-		{
-			*b_number = '0';
-			b_number++;
-		}
-
-	}
-	b_number = ptr;
-
-	write(1, b_number, 8);
-
+	printf("%b", n);
 
 }
