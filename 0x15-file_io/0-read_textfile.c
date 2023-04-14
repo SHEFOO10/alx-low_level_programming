981632c53e6bf601fc96e2d78fc48ec3231a95fc
root <445585913@g102019.moe.edu.eg>
2023-04-14 16:56:08 -0700

    open file and read from it and print content on standrad output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * read_textfile -  reads a text file and prints it to the standard output
+ *
+ * @filename: name of the file.
+ * @letters: amount of letters to red.
+ *
+ * Return: (0) if filename is NULL,
+ *  (0) if write fails or does not write the expected amount of bytes.
+ *  if the file can not be opened or read, return (0)
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o_file, r_file, w_file;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o_file = open(filename, O_RDONLY);
+	r_file = read(o, buffer, letters);
+	w_file = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w_file);
+}
