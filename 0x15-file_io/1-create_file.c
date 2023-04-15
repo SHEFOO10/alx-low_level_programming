49ee66d591d46d6fea73f5914c5c431f89ed1527
root <445585913@g102019.moe.edu.eg>
2023-04-14 17:39:27 -0700

    creates file and insert content to it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ *
+ * @filename: name of the file you are creating.
+ * @text_content: content to be inserted on created file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int o_file, o_write, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+		for (len = 0; text_content[len];)
+			len++;
+	else
+		return (-1);
+
+
+	o_file = open(filename, O_CREAT | O_RDWR, 0600);
+	W_file = write(o_file, text_content, content_len);
+
+	if (o_file == -1 || w_file == -1)
+		return (-1);
+
+	close(o_file);
+	return (1);
+}
