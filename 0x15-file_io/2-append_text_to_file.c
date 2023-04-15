c186a83da106c5c9209261aa3a818e31fc0e8908
root <445585913@g102019.moe.edu.eg>
2023-04-14 19:51:45 -0700

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,8 +24,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	w_file = write(o_file, text_content, content_len);
 	if (o_file == 1)
 		return (1);
-	else if (w_file == -1)
+	else if (o_file == -1 || w_file == -1)
 		return (-1);
+
 	close(o_file);
+
 	return (1);
 }
