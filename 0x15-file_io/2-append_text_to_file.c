292e1e3e984c702421ddcd903becde4710775f77
root <445585913@g102019.moe.edu.eg>
2023-04-14 20:12:47 -0700

    small changes with big impact

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -30,7 +30,9 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	w_file = write(o_file, text_content, content_len);
 
-	if (o_file == -1 || w_file == -1)
+	if (o_file != -1)
+		return (1);
+	else if (o_file == -1 || w_file == -1)
 		return (-1);
 
 	close(o_file);
