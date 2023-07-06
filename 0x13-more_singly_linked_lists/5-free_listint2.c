b5f24c4764dcc6fa12f230da6db071828237d449
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-07-05 18:15:26 -0700

    Head can be NULL already so i added condition

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,14 +8,16 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *current, *temp;
-
-	temp = *head;
-	while ((current = temp) != NULL)
+	if (*head != NULL)
 	{
-		temp = temp->next;
-		free(current);
-	}
-	*head = NULL;
+		listint_t *current, *temp;
+
+		temp = *head;
+		while ((current = temp) != NULL)
+		{
+			temp = temp->next;
+			free(current);
+		}
+		*head = NULL;
 }
 
