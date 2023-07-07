83f7ea5bcfb57e4d6ad0ff7bfaeef499c747f745
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-07-07 16:01:26 -0700

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_binary = print binary on the console.
+ *
+ * @n: decimal number to be printed on binary.
+ *
+ */
+
+void print_binary(unsigned long int n)
+{
+	int number = n;
+	int mask = n;
+
+	if (n == 0)
+		_putchar('0');
+
+	while (mask)
+	{
+		if (mask & n)
+			_putchar('1');
+		else
+			_putchar('0');
+
+		mask >>= 1;
+	}
+}
+
