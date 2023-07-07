99558eaab387037c9145417ba736031f187e6f1f
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-07-07 16:30:34 -0700

    refactored the code again

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * get_length - get length as binary number
+ *
+ * @n: decimal number to calc length of it's binary
+ *
+ * Return: length of the number.
+ */
+int get_length(unsigned long int n)
+{
+	int length = 0;
+
+	while (n > 0)
+	{
+
+		length++;
+		n >>= 1;
+	}
+	length--;
+
+	return (length);
+}
+
+/**
  * print_binary - print binary number on the console.
  *
  * @n: decimal number to be printed on binary.
@@ -10,11 +32,15 @@
 void print_binary(unsigned long int n)
 {
 	unsigned int mask = n;
+	int length;
+	unsigned long int mask = 1;
 
-	if (n == 0)
-		_putchar('0');
+	length = get_length(n);
 
-	while (mask)
+	if (length > 0)
+		mask <<= length;
+
+	while (mask > 0)
 	{
 		if (mask & n)
 			_putchar('1');
