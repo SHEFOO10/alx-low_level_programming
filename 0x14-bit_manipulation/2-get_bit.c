34f2da59f33ff37bed5f95b29300332bb5b671a5
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-07-07 16:57:26 -0700

    get bit on specific index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,13 @@
 #include "main.h"
 
+#define CHAR_BITS 8 /*size of char*/
+#define INT_BITS (sizeof(unsigned long int) * CHAR_BITS)
+
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > INT_BITS)
+		return (-1);
+
 	unsigned long int mask = 1 << index;
 
 	if (n & mask)
