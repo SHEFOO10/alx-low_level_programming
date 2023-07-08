7179abfe89f73c512e37a74815c58b549da360b7
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-07-07 17:00:13 -0700

    fix betty bugs

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,12 +3,20 @@
 #define CHAR_BITS 8 /*size of char*/
 #define INT_BITS (sizeof(unsigned long int) * CHAR_BITS)
 
+/**
+ * get_bit - get bit on specific index
+ *
+ * @n: number to get bit from
+ * @index: index of the bit to get.
+ *
+ * Return: value of the bit on specific index.
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int mask = 1 << index;
+
 	if (index > INT_BITS)
 		return (-1);
-
-	unsigned long int mask = 1 << index;
 
 	if (n & mask)
 		return (1);
