e478378e1f59b3aab29080dc4b8a5590e9ff9b7b
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-07-10 14:29:24 -0700

    replace strlen with custom code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,12 +13,19 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int w_2file;
+	int len;
 
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len] != '\0';)
+			len++;
+	}
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	w_2file = write(fd, text_content, strlen(text_content));
+	w_2file = write(fd, text_content, len);
 
 	if (fd == -1 || w_2file == -1)
 		return (-1);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
