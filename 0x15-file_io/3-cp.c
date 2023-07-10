d3885c59fc8965f68cfb8cfc6622dec47e17c052
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-07-10 16:17:00 -0700

    free buffer if error occured

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -64,11 +64,13 @@ int main(int argc, char *argv[])
 	if (from_f == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't Read from %s\n", argv[1]);
+		free(buffer);
 		exit(98);
 	}
 	if (to_f == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		free(buffer);
 		exit(99);
 	}
 	do {
