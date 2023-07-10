74182c46b4a4a9c3f80414a8f7aab128b090b4bd
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-07-10 12:24:11 -0700

    fix Check 10

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -30,7 +30,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	bytes_read = read(fd, buffer, letters);
 
-	bytes_write = write(1, buffer, bytes_read);
+	bytes_write = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_read == -1 || bytes_write == -1 || bytes_read != bytes_write)
 	{
 		free(buffer);
