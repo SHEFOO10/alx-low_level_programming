933811d107beaed596f311595734271a60ed9b68
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-07-10 16:06:46 -0700

    make program copy file content to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,77 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * create_buffer - create buffer for the second file.
+ *
+ * @filename: name of the file that i would write to.
+ *
+ * Return: buffer pointer on success, (99) on failure.
+ */
+
+char *create_buffer (char *filename)
+{
+	char *buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+	return (buffer);
+}
+
+
+void c_fd(int fd)
+{
+	int close_fd = close(fd);
+	if (close_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd);
+		exit(100);
+	}
+}
+/**
+ * main - Entry point
+ *
+ * @argc: number of args passed to the program.
+ * @argv: args values.
+ *
+ * Return: (0) on success,
+ */
+
+
+int main(int argc, char *argv[])
+{
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s from_file to_file\n", argv[0]);
+		exit(97);
+	}
+	buffer = create_buffer(argv[2]);
+
+	int from_f = open(argv[1], O_RDONLY);
+	int to_f = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	int r_from = read(from_f, buffer, 1024);
+	if (from_f == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't Read from %s\n", argv[1]);
+		exit(98);
+	}
+	if (to_f == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	do
+	{
+		write(to_f, buffer, r_from);
+		r_from = read(from_f, buffer, 1024);
+	} while (r_from > 0);
+
+	free(buffer);
+	c_fd(from_f);
+	c_fd(to_f);
+	return (0);
+}
