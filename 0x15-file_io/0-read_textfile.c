ab3e6fee58808411e6c8906c75931c5883900646
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-07-10 11:18:31 -0700

    add new condition checks if filename is NULL

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,6 +13,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
 	ssize_t bytes_read;
+
+	if (filename == NULL)
+		return (0);
+
 	int fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
