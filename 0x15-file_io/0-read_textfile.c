1acdeafc847bc2842b326b8d76afb51cd22585ce
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-07-10 11:20:12 -0700

    fix Error : ISO C90 forids mixed declarations

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,11 +13,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
 	ssize_t bytes_read;
+	int fd;
 
 	if (filename == NULL)
 		return (0);
 
-	int fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
