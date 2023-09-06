96a210ed8a2d2ddeea637916e3753ee66014f3ce
Sherif Hamdy <445585913@g102019.moe.edu.eg>
2023-09-06 10:22:38 -0700

    read_textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	ssize_t read_wcount, wordcount;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	read_wcount = read(file, buffer, letters);
+
+	if (read_wcount == -1)
+		return (0);	
+
+	wordcount = write(STDIN_FILENO, buffer, read_wcount);
+
+	if (wordcount == -1 || read_wcount != wordcount)
+		return (0);
+
+	free(buffer);
+	close(file);
+
+	return (wordcount);
+}
+
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
