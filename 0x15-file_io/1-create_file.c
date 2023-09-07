8cfb68f11920aac6514b4fa7600272e84b8e95ff
Sherif Hamdy <445585913@g102019.moe.edu.eg>
2023-09-07 09:26:34 -0700

    fix if text_content is NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,10 +20,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-
-	write_status = write(fd, text_content, _strlen(text_content));
-	if (write_status == -1)
-		return (-1);
+	if(text_content)
+	{
+		write_status = write(fd, text_content, _strlen(text_content));
+		if (write_status == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
