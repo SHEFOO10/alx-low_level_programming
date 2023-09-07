3edad93aa56b4a6ca510a1b13e24a0968a569735
Sherif Hamdy <445585913@g102019.moe.edu.eg>
2023-09-07 16:07:05 -0700

    fix switch

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -31,7 +31,7 @@ int main(int argc, char **argv)
 		if (wread == -1)
 			custom_exit(98, argv[1]);
 
-		w_wrote = write(dest, &buffer, wread);
+		w_wrote = write(dest, buffer, wread);
 		if (w_wrote == -1)
 			custom_exit(99, argv[2]);
 	} while ((wread = read(src, &buffer, sizeof(buffer))) > 0);
