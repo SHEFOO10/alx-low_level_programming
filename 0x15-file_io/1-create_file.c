a7f77fd57173a01857b608ade194f8e48ee06b32
Sherif Hamdy <445585913@g102019.moe.edu.eg>
2023-09-07 09:27:45 -0700

    space required before the open parenthesis '('

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-	if(text_content)
+	if (text_content)
 	{
 		write_status = write(fd, text_content, _strlen(text_content));
 		if (write_status == -1)
