493c4b2764ef803d71bb6e09ac0e5c4a02ca5ef3
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-13 01:36:37 +0300

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -17,6 +17,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node = malloc(sizeof(dlistint_t));
 	new_node->n = n;
 	new_node->next = NULL;
+	new_node->prev = NULL;
 
 	if (tmp_head)
 	{
