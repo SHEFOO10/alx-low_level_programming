99d8831053299b540aafa21774433b33da7705ed
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-12 23:44:13 +0300

    insert at begining

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,14 +12,26 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
+	dlistint_t *tmp_head;
 
+	tmp_head = *head;
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
 	new_node->prev = NULL;
-	new_node->next = *head;
+
+	if (tmp_head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	while (tmp_head->prev != NULL)
+		tmp_head = tmp_head->prev;
+	
+	tmp_head->prev = new_node;
+	new_node->next = tmp_head;
 	*head = new_node;
 
 	return (new_node);
