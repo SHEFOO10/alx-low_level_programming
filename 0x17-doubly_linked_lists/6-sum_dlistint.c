d1506b8f1b837b4d6e396da0dec2f4d4951c4906
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-13 02:49:58 +0300

    6-sum_dlistint.c

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head)
+	{
+		if (head->prev != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+	return (sum);
+}
