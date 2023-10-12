e3db76e4c1f1350aead3e9a4c26453aa419804d9
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-13 02:52:16 +0300

    6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_dlistlint -  sum all the data (n) of a dlistint_t linked list.
+ * sum_dlistint -  sum all the data (n) of a dlistint_t linked list.
  *
  * @head: pointer to head of the linked list.
  *
