8322d5223877847e069f065bfea97633f5acb2f9
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-13 02:42:04 +0300

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -2,15 +2,20 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *tmp = head;
 	unsigned int idx = 0;
 
-	while (tmp)
+	if (head == NULL)
+		return (NULL);
+
+	while (head->prev != NULL)
+		head = head->prev;
+	
+	while (head != NULL)
 	{
 		if (idx == index)
-			return (tmp);
-		tmp = tmp->next;
+			break;
+		head = head->next;
 		idx++;
 	}
-	return (NULL);
+	return (head);
 }
