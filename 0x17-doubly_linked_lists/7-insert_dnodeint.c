dceb91cedea44df5b380a429a8884102bb5e61a1
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-13 21:00:09 +0300

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,6 +18,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
+
+	while (tmp->prev != NULL)
+		tmp = tmp->prev;
 	while (tmp)
 	{
 		if (index == idx)
