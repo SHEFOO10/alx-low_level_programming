e93ac726be0c0085e86775e733e037fd68d65036
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-13 21:49:24 +0300

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -28,6 +28,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			if (tmp->prev)
 			{
 				tmp->prev->next = tmp->next;
+				tmp->next->prev = tmp->prev;
 			}
 			else
 			{
