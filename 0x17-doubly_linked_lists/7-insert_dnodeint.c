5eb1f39f360c2a82e5d6e56554342491f0106ddb
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-13 21:03:48 +0300

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,6 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				new_node->n = n;
 				new_node->next = tmp->next;
 				new_node->prev = tmp;
+				tmp->next->prev = new_node;
 				tmp->next = new_node;
 			}
 			break;
