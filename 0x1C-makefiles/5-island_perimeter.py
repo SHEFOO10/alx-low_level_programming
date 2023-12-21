0542ce78fe9edc86a77bee93be79d0430896835c
Sherif Hamdy <445585913@g102019.moe.edu.eg>
2023-12-21 14:36:29 -0800

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,64 +1,21 @@
 #!/usr/bin/python3
-""" 5. Island Perimeter """
-
-
-def calc_width(arrays):
-    """ return array have values n + 1 """
-    flitered_arrays = []
-    last_iterate_index = 0
-    idx = 0
-    arr_idx = 0
-    for arr in arrays:
-        idx = 0
-        for index in range(len(arr)):
-            try:
-                if not (arr[index + 1] and arr[index + 1] == arr[index] + 1):
-                    raise KeyError()
-            except (KeyError, IndexError):
-                idx += 1
-                break
-            idx += 1
-        if last_iterate_index < idx:
-            last_iterate_index = idx
-        arr_idx += 1
-
-    return last_iterate_index
-
-
-def calc_height(island_dict):
-    """ calc height of the island """
-    height = 0
-    for key in island_dict.keys():
-        try:
-            if not (island_dict[key + 1]
-                    and (island_dict[key][0] == island_dict[key + 1][0])):
-                raise KeyError("")
-        except KeyError:
-            break
-        height += 1
-    return height + 1
+""" calculates perimeter of island """
 
 
 def island_perimeter(grid):
-    """ calculate perimeter of island """
-    if grid == [] or grid == [[]]:
+    """ calculates perimeter of island """
+    if (grid == [] or grid == [[]]):
         return 0
-
-    height = 0
-    width = 0
-    common_values_width = []
-    values = dict()
-    for row_index in range(len(grid)):
-        values.update({row_index: list()})
-        for col_index in range(len(grid[row_index])):
-            if grid[row_index][col_index] == 1:
-                values[row_index].append(col_index)
-
-    only_island = {index: value
-                   for index, value in values.items()
-                   if value != []}
-
-    height = calc_height(only_island)
-    only_island = dict(list(only_island.items())[:height])
-    width = calc_width(only_island.values())
-    return (height + width) * 2
+    width = len(grid[0])
+    height = len(grid)
+    size = 0
+    edges = 0
+    for row in range(height):
+        for col in range(width):
+            if grid[row][col] == 1:
+                size += 1
+            if (row > 0 and grid[row - 1][col] == 1):
+                edges += 1
+            if (col > 0 and grid[row][col - 1] == 1):
+                edges += 1
+    return size * 4 - edges * 2
